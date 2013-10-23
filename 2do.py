5b119b86906e78127dd8f4851ed89ddfd436533c
Julien Pecqueur <jpec@julienpecqueur.net>
2013-10-23 19:32:31 +0200

    Refactoring

--- 2do.py
+++ 2do.py
@@ -51,202 +51,13 @@ from tkinter.messagebox import askyesno
 from tkinter.ttk import Button, Frame
 
 
-def isNewDb(dbfile):
-    "Return True if the database doesn't exist"
-    if isfile(dbfile):
-        return(False)
-    else:
-        return(True)
-
-
-def getDbFile():
-    "Return database file"
-    if 'nt' == uname:
-        return(SDBFILE_NT)
-    else:
-        return(SDBFILE_UX)
-
-
-def openDb():
-    "Open the database"
-    path = expanduser(getDbFile())
-    print(path)
-    new = isNewDb(path)
-    db = connect(path)
-    if not new:
-        return(db)
-    elif new and createTables(db):
-        return(db)
-    else:
-        return(None)
-
-
-def createTables(db):
-    "Create the database's tables"
-    sql = "CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, "\
-          +"task TEXT, active INT, done INT, urgent INT);"
-    db.execute(sql)
-    db.commit()
-    return(True)
-
-
-def addTask(db, task):
-    "Add the task in the database"
-    sql = "INSERT INTO tasks (task, active, done, urgent) VALUES (?, 1, 0, 0);"
-    id = db.execute(sql, (task, )).lastrowid
-    db.commit()
-    return(id)
-
-
-def activeTask(db, id, active):
-    "Set active flag for the task"
-    sql = "UPDATE tasks SET active = ? WHERE id = ? ;"
-    db.execute(sql, (active, id))
-    db.commit()
-    return(True)
-
-
-def doneTask(db, id, done):
-    "Set done flag for the task"
-    sql = "UPDATE tasks SET done = ? WHERE id = ? ;"
-    db.execute(sql, (done, id))
-    db.commit()
-    return(True)
-
-
-def urgentTask(db, id, urgent):
-    "Set urgent flag for the task"
-    sql = "UPDATE tasks SET urgent = ? WHERE id = ? ;"
-    db.execute(sql,(urgent, id))
-    db.commit()
-    return(True)
-
-
-def editTask(db, id, new):
-    "Update the task"
-    sql = "UPDATE tasks SET task = ? WHERE id = ? ;"
-    id = db.execute(sql, (new, id))
-    db.commit()
-    return(True)
-
-
-def getTasks(db, archives):
-    "Get the tasks list"
-    sql = "SELECT id, task, active, done, urgent FROM tasks WHERE active = ? ;"
-    if archives:
-        r = db.execute(sql, (0, ))
-    else:
-        r = db.execute(sql, (1, ))
-    return(r.fetchall())
-
-
-def getTaskInfos(db, id):
-    "Get task's infos"
-    sql = "SELECT id, task, active, done, urgent FROM tasks WHERE id = ? ;"
-    r = db.execute(sql, (id, ))
-    t = dict()
-    for id, task, active, done, urgent in r.fetchall():
-        t['id'] = id
-        t['task'] = task
-        t['active'] = int(active)
-        t['done'] = int(done)
-        t['urgent'] = int(urgent)
-        return(t)
-
-
-def getTask(db, id):
-    "Get a task"
-    t = getTaskInfos(db, id)
-    return(t['task'])
-
-
-def isUrgent(db, id):
-    "Get urgent flag for the task"
-    t = getTaskInfos(db, id)
-    return(t['urgent'])
-
-
-def isDone(db, id):
-    "Get done flag for the task"
-    t = getTaskInfos(db, id)
-    return(t['done'])
-
-
-def isNotArchive(db, id):
-    "Get the *active* flag for the task"
-    t = getTaskInfos(db, id)
-    return(t['active'])
-
-
-def load(app, archives):
-    "Load the tasks list"
-    l = getTasks(app.db, archives)
-    i = 0
-    for id, task, active, done, urgent in l:
-        if int(done) > 0:
-            lbl = "{0} (done)".format(task)
-        else:
-            lbl = "{0}".format(task)
-        app.ui.lb.insert(id, lbl)
-        if int(done) > 0:
-            app.ui.lb.itemconfig(i, fg='grey')
-        elif int(urgent) > 0:
-            app.ui.lb.itemconfig(i, fg='red')
-        else:
-            app.ui.lb.itemconfig(i, fg='black')
-        app.tasks[i] = id
-        i = i+1
-    return(True)
-
-
-def drawUi(app):
-    "Draw the UI"
-    ui = Tk()
-    ui.title("{0} {1}".format(app.program, app.version))
-    # toolbar
-    ui.tb = Frame(ui)
-    ui.tb.pack(anchor='nw')
-    # tools
-    ui.tb.new = Button(ui.tb, text="New", width=8, command=app.new)
-    ui.tb.new.grid(row=1, column=0, padx=2, pady=2)
-    ui.tb.edi = Button(ui.tb, text="Edit", width=8, command=app.edi)
-    ui.tb.edi.grid(row=1, column=1, padx=2, pady=2)
-    ui.tb.arc = Button(ui.tb, text="Archive", width=8, command=app.arc)
-    ui.tb.arc.grid(row=1, column=5, padx=2, pady=2)
-    ui.tb.don = Button(ui.tb, text="Status", width=8, command=app.don)
-    ui.tb.don.grid(row=1, column=2, padx=2, pady=2)
-    ui.tb.urg = Button(ui.tb, text="Urgent", width=8, command=app.urg)
-    ui.tb.urg.grid(row=1, column=3, padx=2, pady=2)
-    ui.tb.vis = Button(ui.tb, text="View bin", width=10, command=app.vis)
-    ui.tb.vis.grid(row=1, column=6, padx=2, pady=2)
-    # listbox
-    ui.lb = Listbox(ui, selectmode=EXTENDED)
-    ui.lb.pack(expand=True, fill='both')
-    # statusbar
-    ui.sb = Frame(ui)
-    ui.sb.pack(anchor='sw')
-    ui.sb.log = Label(ui.sb)
-    ui.sb.log.pack(expand=True, fill='both')
-    ui.bind("<n>", app.evtNew)
-    ui.bind("<e>", app.evtEdi)
-    ui.bind("<a>", app.evtArc)
-    ui.bind("<v>", app.evtVis)
-    ui.bind("<s>", app.evtDon)
-    ui.bind("<u>", app.evtUrg)
-    ui.bind("<h>", app.evtHel)
-    ui.lb.bind("<Double-Button-1>", app.evtEdi)
-    ui.lb.bind("<Button-2>", app.evtUrg)
-    ui.lb.bind("<Double-Button-3>", app.evtDon)
-    return(ui)
-
-
 class app(object):
     "Classe app for 2do"
 
     def __init__(self, program, version, dochelp):
         "Initialize the class"
         # open/create the database
-        self.db = openDb()
+        self.db = self.openDb()
         # init variables
         self.program = program
         self.version = version
@@ -256,10 +67,10 @@ class app(object):
         self.archives = None
         if self.db:
             # launch app
-            self.ui = drawUi(self)
+            self.ui = self.drawUi()
             self.log("Loading data…")
             # load tasks
-            load(self, self.archives)
+            self.load(self.archives)
             self.log("Data loaded !")
             self.ui.lb.focus_set()
             self.log("Press <h> to display help…")
@@ -312,7 +123,7 @@ class app(object):
         self.log("Appending new task…")
         task = askstring("New task ?", "Enter the new task :")
         if task:
-            id = addTask(self.db, task)
+            id = self.addTask(task)
             if id:
                 self.log("Task {0} added !".format(id))
                 self.reload(self.archives)
@@ -331,11 +142,11 @@ class app(object):
         ids = self.ui.lb.curselection()
         for task in ids:
             id = self.tasks[task]
-            old = getTask(self.db, id)
+            old = self.getTask(id)
             self.log("Editing task {0}…".format(id))
             new = askstring("Edit task ?", "Enter the new task :", initialvalue=old)
             if new:
-                editTask(self.db, id, new)
+                self.editTask(id, new)
                 self.log("Task {0} edited !".format(id))
         self.reload(self.archives)
 
@@ -350,12 +161,12 @@ class app(object):
         ids = self.ui.lb.curselection()
         for task in ids:
             id = self.tasks[task]
-            if isNotArchive(self.db, id) and \
+            if self.isNotArchive(id) and \
                askyesno("Archive ?", "Do yo want to archive task {0} ?".format(id)):
-                activeTask(self.db, id, 0)
+                self.activeTask(id, 0)
                 self.log("Task {0} archived !".format(id))
             else:
-                activeTask(self.db, id, 1)
+                self.activeTask(id, 1)
                 self.log("Task {0} un-archived !".format(id))
         self.reload(self.archives)
 
@@ -371,11 +182,11 @@ class app(object):
         ids = self.ui.lb.curselection()
         for task in ids:
             id = self.tasks[task]
-            if isDone(self.db, id):
-                doneTask(self.db, id, 0)
+            if self.isDone(id):
+                self.doneTask(id, 0)
                 self.log("Task {0} un-done !".format(id))
             else:
-                doneTask(self.db, id, 1)
+                self.doneTask(id, 1)
                 self.log("Task {0} done !".format(id))
         self.reload(self.archives)
 
@@ -391,11 +202,11 @@ class app(object):
         ids = self.ui.lb.curselection()
         for task in ids:
             id = self.tasks[task]
-            if isUrgent(self.db, id):
-                urgentTask(self.db, id, 0)
+            if self.isUrgent(id):
+                self.urgentTask(id, 0)
                 self.log("Task {0} is not urgent !".format(id))
             else:
-                urgentTask(self.db, id, 1)
+                self.urgentTask(id, 1)
                 self.log("Task {0} is urgent !".format(id))
         self.reload(self.archives)
 
@@ -405,9 +216,9 @@ class app(object):
         self.ui.lb.delete(0, END)
         self.tasks = dict()
         if archives:
-            load(self, True)
+            self.load(True)
         else:
-            load(self, False)
+            self.load(False)
         self.archives = archives
         self.ui.lb.focus_set()
         self.ui.lb.selection_set(END)
@@ -423,6 +234,195 @@ class app(object):
     def log(self, msg):
         "Display log in status bar"
         self.ui.sb.log.configure(text="> {0}".format(msg))
+
+
+    def isNewDb(self, dbfile):
+        "Return True if the database doesn't exist"
+        if isfile(dbfile):
+            return(False)
+        else:
+            return(True)
+
+
+    def getDbFile(self):
+        "Return database file"
+        if 'nt' == uname:
+            return(SDBFILE_NT)
+        else:
+            return(SDBFILE_UX)
+
+
+    def openDb(self):
+        "Open the database"
+        path = expanduser(self.getDbFile())
+        print(path)
+        new = self.isNewDb(path)
+        db = connect(path)
+        if not new:
+            return(db)
+        elif new and self.createTables(db):
+            return(db)
+        else:
+            return(None)
+
+
+    def createTables(self):
+        "Create the database's tables"
+        sql = "CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, "\
+              +"task TEXT, active INT, done INT, urgent INT);"
+        self.db.execute(sql)
+        self.db.commit()
+        return(True)
+
+
+    def addTask(self, task):
+        "Add the task in the database"
+        sql = "INSERT INTO tasks (task, active, done, urgent) VALUES (?, 1, 0, 0);"
+        id = self.db.execute(sql, (task, )).lastrowid
+        self.db.commit()
+        return(id)
+
+
+    def activeTask(self, id, active):
+        "Set active flag for the task"
+        sql = "UPDATE tasks SET active = ? WHERE id = ? ;"
+        self.db.execute(sql, (active, id))
+        self.db.commit()
+        return(True)
+
+
+    def doneTask(self, id, done):
+        "Set done flag for the task"
+        sql = "UPDATE tasks SET done = ? WHERE id = ? ;"
+        self.db.execute(sql, (done, id))
+        self.db.commit()
+        return(True)
+
+
+    def urgentTask(self, id, urgent):
+        "Set urgent flag for the task"
+        sql = "UPDATE tasks SET urgent = ? WHERE id = ? ;"
+        self.db.execute(sql,(urgent, id))
+        self.db.commit()
+        return(True)
+
+
+    def editTask(self, id, new):
+        "Update the task"
+        sql = "UPDATE tasks SET task = ? WHERE id = ? ;"
+        self.db.execute(sql, (new, id))
+        self.db.commit()
+        return(True)
+
+
+    def getTasks(self, archives):
+        "Get the tasks list"
+        sql = "SELECT id, task, active, done, urgent FROM tasks WHERE active = ? ;"
+        if archives:
+            r = self.db.execute(sql, (0, ))
+        else:
+            r = self.db.execute(sql, (1, ))
+        return(r.fetchall())
+
+
+    def getTaskInfos(self, id):
+        "Get task's infos"
+        sql = "SELECT id, task, active, done, urgent FROM tasks WHERE id = ? ;"
+        r = self.db.execute(sql, (id, ))
+        t = dict()
+        for id, task, active, done, urgent in r.fetchall():
+            t['id'] = id
+            t['task'] = task
+            t['active'] = int(active)
+            t['done'] = int(done)
+            t['urgent'] = int(urgent)
+            return(t)
+
+
+    def getTask(self, id):
+        "Get a task"
+        t = self.getTaskInfos(id)
+        return(t['task'])
+
+
+    def isUrgent(self, id):
+        "Get urgent flag for the task"
+        t = self.getTaskInfos(id)
+        return(t['urgent'])
+
+
+    def isDone(self, id):
+        "Get done flag for the task"
+        t = self.getTaskInfos(id)
+        return(t['done'])
+
+
+    def isNotArchive(self, id):
+        "Get the *active* flag for the task"
+        t = self.getTaskInfos(id)
+        return(t['active'])
+
+
+    def load(self, archives):
+        "Load the tasks list"
+        l = self.getTasks(archives)
+        i = 0
+        for id, task, active, done, urgent in l:
+            if int(done) > 0:
+                lbl = "{0} (done)".format(task)
+            else:
+                lbl = "{0}".format(task)
+            self.ui.lb.insert(id, lbl)
+            if int(done) > 0:
+                self.ui.lb.itemconfig(i, fg='grey')
+            elif int(urgent) > 0:
+                self.ui.lb.itemconfig(i, fg='red')
+            else:
+                self.ui.lb.itemconfig(i, fg='black')
+            self.tasks[i] = id
+            i = i+1
+        return(True)
+
+
+    def drawUi(self):
+        "Draw the UI"
+        ui = Tk()
+        ui.title("{0} {1}".format(self.program, self.version))
+        # toolbar
+        ui.tb = Frame(ui)
+        ui.tb.pack(anchor='nw')
+        # tools
+        ui.tb.new = Button(ui.tb, text="New", width=8, command=self.new)
+        ui.tb.new.grid(row=1, column=0, padx=2, pady=2)
+        ui.tb.edi = Button(ui.tb, text="Edit", width=8, command=self.edi)
+        ui.tb.edi.grid(row=1, column=1, padx=2, pady=2)
+        ui.tb.arc = Button(ui.tb, text="Archive", width=8, command=self.arc)
+        ui.tb.arc.grid(row=1, column=5, padx=2, pady=2)
+        ui.tb.don = Button(ui.tb, text="Status", width=8, command=self.don)
+        ui.tb.don.grid(row=1, column=2, padx=2, pady=2)
+        ui.tb.urg = Button(ui.tb, text="Urgent", width=8, command=self.urg)
+        ui.tb.urg.grid(row=1, column=3, padx=2, pady=2)
+        ui.tb.vis = Button(ui.tb, text="View bin", width=10, command=self.vis)
+        ui.tb.vis.grid(row=1, column=6, padx=2, pady=2)
+        # listbox
+        ui.lb = Listbox(ui, selectmode=EXTENDED)
+        ui.lb.pack(expand=True, fill='both')
+        # statusbar
+        ui.sb = Frame(ui)
+        ui.sb.pack(anchor='sw')
+        ui.sb.log = Label(ui.sb)
+        ui.sb.log.pack(expand=True, fill='both')
+        ui.bind("<n>", self.evtNew)
+        ui.bind("<e>", self.evtEdi)
+        ui.bind("<a>", self.evtArc)
+        ui.bind("<v>", self.evtVis)
+        ui.bind("<s>", self.evtDon)
+        ui.bind("<u>", self.evtUrg)
+        ui.bind("<h>", self.evtHel)
+        ui.lb.bind("<Double-Button-1>", self.evtEdi)
+        ui.lb.bind("<Button-2>", self.evtUrg)
+        ui.lb.bind("<Double-Button-3>", self.evtDon)
+        return(ui)
 
 
 if __name__ == '__main__':
