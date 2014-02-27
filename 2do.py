bf730ed261ee55018823d5ff2eb61b94ddc0c3d9
Julien Pecqueur <jpec80@gmail.com>
2014-02-27 13:23:27 +0100

    2do.py v2.0-alpha
    Add :
    - console
    - first level tag
    - second level tag

--- 2do.py
+++ 2do.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-PROGRAM = "2do « TODO LIST MANAGER »"
-VERSION = "v1.3"
+PROGRAM = "2do task manager"
+VERSION = "v2.0-alpha"
 DOCHELP = """
 {0} {1}
 --
@@ -10,7 +10,7 @@ This software is a simple todo list manager.
 It's written in Python using Tkinter and Sqlite.
 --
 Author: Julien Pecqueur
-Home: http://github.com/jpec/2do.py
+Home: http://github.com/jpec/2do
 Email: jpec@julienpecqueur.net
 License: GPL
 --
@@ -26,6 +26,8 @@ Keyboard shortcuts on main window:
  <f> Filter box
  <h> Help
  <F5> Reload data (refresh)
+ <F11> Open console
+ 
 
 Keyboard shortcuts in trash:
  <t> view Tasks
@@ -37,13 +39,27 @@ Keyboard shortcuts in filter:
 
 """.format(PROGRAM, VERSION)
 
+# TAGs first level :
+F1 = "BUGS"
+F2 = "FEAT"
+F3 = "IDEA"
+F4 = "WONT"
+F5 = "POST"
+
+# TAGs second level :
+F6 = "DEV1"
+F7 = "DEV2"
+F8 = "DEV3"
+F9 = "DEV4"
+F0 = "DEV5"
+
 
 #---------------------------------------------------------------------
 # PATHS FOR DB FILE
 #---------------------------------------------------------------------
 
 # If system is Window :
-SDBFILE_NT = "~/2do.db"
+SDBFILE_NT = "c:/tmp/2do.db"
 
 # If system is Unix :
 SDBFILE_UX = "~/.2do.db"
@@ -54,20 +70,77 @@ SDBFILE_UX = "~/.2do.db"
 #---------------------------------------------------------------------
 
 from sqlite3 import connect
+from sqlite3 import Error as SqlError
 from os import name as uname
 from os.path import isfile
 from os.path import expanduser
 from tkinter import *
+from tkinter.scrolledtext import ScrolledText
 from tkinter.simpledialog import askstring
 from tkinter.messagebox import showinfo
+from tkinter.messagebox import showerror
 from tkinter.messagebox import askyesno
 from tkinter.ttk import Button, Frame, Entry
 
 
+class console(object):
+    
+    "Class for console"
+    
+    def __init__(self, db, master):
+        "Start the console"
+        self.db = db
+        self.master = master
+        self.ui = self.draw_ui()
+        self.ui.term.insert(END ,"Type your request and press <Enter>.\n")
+        self.ui.mainloop()
+    
+    def execute(self, event=None):
+        "Execute request"
+        sql = self.ui.cmd.get()
+        self.ui.cmd.configure(state = 'disabled')
+        res = False
+        if sql:
+            try:
+                sql = sql.strip()
+                cur = self.db.execute(sql)
+                self.ui.term.insert(END ,">>> {0}\n".format(sql))
+                self.db.commit()
+                if sql.lstrip().upper().startswith("SELECT"):
+                    res = cur.fetchall()
+            except SqlError as e:
+                msg = "{0}".format(e.args[0])
+                #showerror("Erreur !", msg)
+                self.ui.term.insert(END ,"{0}\n".format(msg))
+        #self.ui.term.insert(END ,">>> {0}\n".format(sql))
+        self.ui.term.see(END)
+        if res:
+            for l in res:
+                l=str(l)
+                self.ui.term.insert(END ,l+"\n")
+                self.ui.term.see(END)
+        self.ui.cmd.configure(state = 'normal')
+        self.ui.cmd.focus()
+        
+
+    def draw_ui(self):
+        "Draw the UI"
+        ui = Tk()
+        ui.title("console")
+        # Console
+        ui.term = ScrolledText(ui)
+        ui.term.pack(fill=BOTH, expand=1)
+        # Barre commande
+        ui.cmd = Entry(ui)
+        ui.cmd.pack(side=LEFT, fill=BOTH, expand=1)
+        ui.bind('<Return>', self.execute)
+        ui.cmd.focus()
+        return(ui)
+
 
 class app(object):
 
-    "Classe app for 2do"
+    "Class app for 2do"
 
     def __init__(self, program, version, dochelp):
         "Initialize the class"
@@ -100,11 +173,17 @@ class app(object):
             print("Database error !")
 
 
-    def evtF5(self, event):
-        "Event F5 - Reload"
+    def evtRef(self, event):
+        "Event Refresh - Reload"
         self.log("Reloading data…")
         self.reload(self.archives)
         self.log("Data reloaded!")
+
+
+    def evtCon(self, event):
+        "Event - open console"
+        console(self.db, self.ui)
+
 
     def evtVis(self, event):
         "Event toggle tasks/archive mode"
@@ -125,6 +204,16 @@ class app(object):
             self.ui.tb.don.configure(state=DISABLED)
             self.ui.tb.urg.configure(state=DISABLED)
             self.ui.fb.src.configure(state=DISABLED)
+            self.ui.tg.tf1.configure(state=DISABLED)
+            self.ui.tg.tf2.configure(state=DISABLED)
+            self.ui.tg.tf3.configure(state=DISABLED)
+            self.ui.tg.tf4.configure(state=DISABLED)
+            self.ui.tg.tf5.configure(state=DISABLED)
+            self.ui.tg.tf6.configure(state=DISABLED)
+            self.ui.tg.tf7.configure(state=DISABLED)
+            self.ui.tg.tf8.configure(state=DISABLED)
+            self.ui.tg.tf9.configure(state=DISABLED)
+            self.ui.tg.tf0.configure(state=DISABLED)
             self.log("Displaying the archives bin…")
         else:
             # archives mode
@@ -138,6 +227,16 @@ class app(object):
             self.ui.tb.don.configure(state=NORMAL)
             self.ui.tb.urg.configure(state=NORMAL)
             self.ui.fb.src.configure(state=NORMAL)
+            self.ui.tg.tf1.configure(state=NORMAL)
+            self.ui.tg.tf2.configure(state=NORMAL)
+            self.ui.tg.tf3.configure(state=NORMAL)
+            self.ui.tg.tf4.configure(state=NORMAL)
+            self.ui.tg.tf5.configure(state=NORMAL)
+            self.ui.tg.tf6.configure(state=NORMAL)
+            self.ui.tg.tf7.configure(state=NORMAL)
+            self.ui.tg.tf8.configure(state=NORMAL)
+            self.ui.tg.tf9.configure(state=NORMAL)
+            self.ui.tg.tf0.configure(state=NORMAL)
             self.log("Displaying the tasks…")
 
 
@@ -222,20 +321,6 @@ class app(object):
             self.log("Tesk {0} copied.".format(id))
 
 
-    def getTaskInfos(self, id):
-        "Get task's infos"
-        sql = "SELECT id, task, active, done, urgent FROM tasks WHERE id = ? ;"
-        r = self.db.execute(sql, (id, ))
-        t = dict()
-        for id, task, active, done, urgent in r.fetchall():
-            t['id'] = id
-            t['task'] = task
-            t['active'] = int(active)
-            t['done'] = int(done)
-            t['urgent'] = int(urgent)
-            return(t)
-
-
     def evtEdi(self, event):
         "Event edit task(s)"
         if not self.archives:
@@ -256,6 +341,78 @@ class app(object):
         self.reload(self.archives, task=id)
 
 
+    def evtF1(self, event):
+        "Event tag milestone 1"
+        if not self.archives:
+            self.tag('milestone', F1)
+
+
+    def evtF2(self, event):
+        "Event tag milestone 2"
+        if not self.archives:
+            self.tag('milestone', F2)
+
+
+    def evtF3(self, event):
+        "Event tag milestone 3"
+        if not self.archives:
+            self.tag('milestone', F3)
+
+
+    def evtF4(self, event):
+        "Event tag milestone 4"
+        if not self.archives:
+            self.tag('milestone', F4)
+
+
+    def evtF5(self, event):
+        "Event tag milestone 5"
+        if not self.archives:
+            self.tag('milestone', F5)
+
+
+    def evtF6(self, event):
+        "Event tag team 1"
+        if not self.archives:
+            self.tag('team', F6)
+
+
+    def evtF7(self, event):
+        "Event tag team 2"
+        if not self.archives:
+            self.tag('team', F7)
+
+
+    def evtF8(self, event):
+        "Event tag team 3"
+        if not self.archives:
+            self.tag('team', F8)
+
+
+    def evtF9(self, event):
+        "Event tag team 4"
+        if not self.archives:
+            self.tag('team', F9)
+
+
+    def evtF0(self, event):
+        "Event tag team 5"
+        if not self.archives:
+            self.tag('team', F0)
+
+        
+    def tag(self, tag, value):
+        "Tag milestone/team"
+        ids = self.ui.lb.curselection()
+        for task in ids:
+            id = self.tasks[str(task)]
+            if self.tagTask(id, tag, value):
+                self.log("Task {0} tagged for {1} !".format(id, value))
+            else:
+                self.log("Error happened when tagging {0} for {1} !".format(id, value))
+        self.reload(self.archives, task=id)
+        
+        
     def evtArc(self, event):
         "Event toggle archive flag"
         self.arc()
@@ -396,7 +553,7 @@ class app(object):
     def createTables(self, db):
         "Create the database's tables"
         sql = "CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, "\
-              +"task TEXT, active INT, done INT, urgent INT);"
+              +"task TEXT, milestone TEXT, active INT, done INT, urgent INT, team TEXT);"
         db.execute(sql)
         db.commit()
         self.db = db
@@ -405,11 +562,24 @@ class app(object):
 
     def addTask(self, task):
         "Add the task in the database"
-        sql = "INSERT INTO tasks (task, active, done, urgent) VALUES (?, 1, 0, 0);"
+        sql = "INSERT INTO tasks (task, milestone, team, active, done, urgent) VALUES (?, '', '', 1, 0, 0);"
         id = self.db.execute(sql, (task, )).lastrowid
         self.db.commit()
         return(id)
 
+
+    def tagTask(self, id, tag, value):
+        "Set the milestone/team for the task"
+        if tag == "milestone":
+            sql = "UPDATE tasks SET milestone = ? WHERE id = ? ;"
+        elif tag == "team":
+            sql = "UPDATE tasks SET team = ? WHERE id = ? ;"
+        else:
+            return(False)
+        self.db.execute(sql, (value, id))
+        self.db.commit()
+        return(True)
+    
 
     def activeTask(self, id, active):
         "Set active flag for the task"
@@ -446,22 +616,27 @@ class app(object):
     def getTasks(self, archives, mask="%"):
         "Get the tasks list"
         if archives:
-            sql = "SELECT id, task, active, done, urgent FROM tasks WHERE active = ? AND task LIKE ?;"
+            sql = "SELECT id, task, milestone, active, done, urgent, team FROM tasks WHERE active = ? AND task LIKE ?;"
             r = self.db.execute(sql, (0, mask))
         else:
-            sql = "SELECT id, task, active, done, urgent FROM tasks WHERE active = ? AND task LIKE ? ORDER BY task, id;"
-            r = self.db.execute(sql, (1, mask))
+            sql  = "SELECT id, task, milestone, active, done, urgent, team "
+            sql += "FROM tasks "
+            sql += "WHERE active = ? AND ( task LIKE ? OR team LIKE ? OR milestone LIKE ? ) "
+            sql += "ORDER BY milestone, task, id;"
+            r = self.db.execute(sql, (1, mask, mask, mask))
         return(r.fetchall())
 
 
     def getTaskInfos(self, id):
         "Get task's infos"
-        sql = "SELECT id, task, active, done, urgent FROM tasks WHERE id = ? ;"
+        sql = "SELECT id, task, milestone, active, done, urgent FROM tasks WHERE id = ? ;"
         r = self.db.execute(sql, (id, ))
         t = dict()
-        for id, task, active, done, urgent in r.fetchall():
+        for id, task, milestone, active, done, urgent in r.fetchall():
+            print(id, task, milestone, active,done, urgent)
             t['id'] = id
             t['task'] = task
+            t['milestone'] = milestone
             t['active'] = int(active)
             t['done'] = int(done)
             t['urgent'] = int(urgent)
@@ -496,11 +671,11 @@ class app(object):
         "Load the tasks list"
         l = self.getTasks(archives, self.mask.get())
         i = 0
-        for id, task, active, done, urgent in l:
+        for id, task, milestone, active, done, urgent, team in l:
             if archives:
                 lbl = "[{0}] {1}".format(id, task)
             else:
-                lbl = "{0}".format(task)
+                lbl = "[{0}] {1} ({2})".format(milestone, task, team)
             self.ui.lb.insert(i, lbl)
             if int(done) > 0:
                 self.ui.lb.itemconfig(i, fg='grey')
@@ -545,8 +720,35 @@ class app(object):
         ui.tb.urg.pack(side=LEFT, padx=2, pady=2)
         ui.tb.arc = Button(ui.tb, text="Remove", width=wb, command=self.arc)
         ui.tb.arc.pack(side=LEFT, padx=2, pady=2)
+        ui.tb.ref = Button(ui.tb, text="Refresh", width=wb, command=lambda:self.evtRef(None))
+        ui.tb.ref.pack(side=LEFT, padx=2, pady=2)
         ui.tb.vis = Button(ui.tb, text="Trash", width=wb, command=self.vis)
-        ui.tb.vis.pack(side=LEFT, padx=2, pady=2)
+        ui.tb.vis.pack(side=RIGHT, padx=2, pady=2)
+        ui.tb.cmd = Button(ui.tb, text="Console", width=wb, command=lambda:self.evtCon(None))
+        ui.tb.cmd.pack(side=RIGHT, padx=2, pady=2)
+        # tagbar
+        ui.tg = Frame(ui)
+        ui.tg.pack(fill=X)
+        ui.tg.tf1 = Button(ui.tg, text=F1, width=wb, command=lambda:self.evtF1(None))
+        ui.tg.tf1.pack(side=LEFT, padx=2, pady=2)
+        ui.tg.tf2 = Button(ui.tg, text=F2, width=wb, command=lambda:self.evtF2(None))
+        ui.tg.tf2.pack(side=LEFT, padx=2, pady=2)
+        ui.tg.tf3 = Button(ui.tg, text=F3, width=wb, command=lambda:self.evtF3(None))
+        ui.tg.tf3.pack(side=LEFT, padx=2, pady=2)
+        ui.tg.tf4 = Button(ui.tg, text=F4, width=wb, command=lambda:self.evtF4(None))
+        ui.tg.tf4.pack(side=LEFT, padx=2, pady=2)
+        ui.tg.tf5 = Button(ui.tg, text=F5, width=wb, command=lambda:self.evtF5(None))
+        ui.tg.tf5.pack(side=LEFT, padx=2, pady=2)
+        ui.tg.tf6 = Button(ui.tg, text=F6, width=wb, command=lambda:self.evtF6(None))
+        ui.tg.tf6.pack(side=LEFT, padx=2, pady=2)
+        ui.tg.tf7 = Button(ui.tg, text=F7, width=wb, command=lambda:self.evtF7(None))
+        ui.tg.tf7.pack(side=LEFT, padx=2, pady=2)
+        ui.tg.tf8 = Button(ui.tg, text=F8, width=wb, command=lambda:self.evtF8(None))
+        ui.tg.tf8.pack(side=LEFT, padx=2, pady=2)
+        ui.tg.tf9 = Button(ui.tg, text=F9, width=wb, command=lambda:self.evtF9(None))
+        ui.tg.tf9.pack(side=LEFT, padx=2, pady=2)
+        ui.tg.tf0 = Button(ui.tg, text=F0, width=wb, command=lambda:self.evtF0(None))
+        ui.tg.tf0.pack(side=LEFT, padx=2, pady=2)
         # filterbar
         ui.fb = Frame(ui)
         ui.fb.pack(fill=X)
@@ -566,7 +768,8 @@ class app(object):
         ui.sb.pack(fill=X)
         ui.sb.log = Label(ui.sb)
         ui.sb.log.pack(expand=True, fill='both', padx=2, pady=2)
-        ui.bind("<F5>", self.evtF5)
+        ui.bind("<F5>", self.evtRef)
+        ui.bind("<F11>", self.evtCon)
         ui.lb.bind("<n>", self.evtNew)
         ui.lb.bind("<c>", self.evtCop)
         ui.lb.bind("<p>", self.evtPas)
